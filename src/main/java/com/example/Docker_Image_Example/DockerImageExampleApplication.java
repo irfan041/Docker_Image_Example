65dd56edd36a530aaff41778cc71904a18d6c5fb
irfan041 <er.irfan041@gmail.com>
2022-06-23 14:00:54 -0400

    project uploaded

---
+++ src/main/java/com/example/Docker_Image_Example/DockerImageExampleApplication.java
@@ -0,0 +1,23 @@
+package com.example.Docker_Image_Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class DockerImageExampleApplication {
+	
+	@GetMapping("/hello")
+	public String getMessage() {
+		return "Welcome to docker container";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerImageExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Docker_Image_Example/DockerImageExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Docker_Image_Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
